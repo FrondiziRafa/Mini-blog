1765c418a386fd26badd68e150916c3ff3f19f2a
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-05-28 12:03:18 -0300

    Configurando o Router

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,8 @@
+// CSS
+import styles from './About.module.css'
+
+export const About = () => {
+  return (
+    <div>About</div>
+  )
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+// CSS
+import styles from './Home.module.css'
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
