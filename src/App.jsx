119b55a6312e5e44f549fec5374b405a9a1bedb2
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-05-29 22:04:16 -0300

    Links para usuários autenticados

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 import { Login } from './Login/Login'
 import { Register } from './Register/Register'
+import { CreatePost } from './pages/CreatePost/CreatePost';
+import { Dashboard } from './pages/Dashboard/Dashboard';
 
 function App() {
   
@@ -43,7 +45,7 @@ function App() {
 
   return (
       <div className='App'>
-        <AuthProvider value={ user }>
+        <AuthProvider value={user}>
           <BrowserRouter>
           <Navbar />
             <div className='container'>
@@ -52,6 +54,8 @@ function App() {
                 <Route path='/about' element={<About />}/>
                 <Route path='/login' element={<Login />}/>
                 <Route path='/register' element={<Register/>}/>
+                <Route path='/posts/create' element={<CreatePost />} />
+                <Route path='/dashboard' element={<Dashboard />}/>
               </Routes>
             </div>
             <Footer />
