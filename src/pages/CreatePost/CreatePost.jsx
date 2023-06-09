72f639805bcae97c7c51dc3bed40801e99a96fe3
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-06-09 13:36:25 -0300

    Criando post

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -3,6 +3,7 @@ import styles from './CreatePost.module.css'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocuments';
 
 export const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,30 @@ export const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const { user } = useAuthValue()
+
+  const { insertDocument, response } = useInsertDocument('posts');
+
   const handleSubmit = (e) => {
     e.preventDefault()
+    setFormError("")
+
+    // validate image URL
+
+    // create an array of tags
+
+    // check all the values
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName
+    });
+
+    // redirect to home page
   }
 
 
@@ -71,9 +94,9 @@ export const CreatePost = () => {
             value={tags}
             />
           </label>
-          {/* {!loading && <button className='btn'>Entrar</button>}
-          {loading && <button className='btn' disabled>Aguarde...</button>}
-            {error && <p className='error'>{error}</p>} */}
+          {!response.loading && <button className='btn' onClick={()=> handleSubmit}>Criar</button>}
+          {response.loading && <button className='btn' disabled>Aguarde...</button>}
+          {response.error && <p className='error'>{response.error}</p>}
         </form>
     </div>
   )
